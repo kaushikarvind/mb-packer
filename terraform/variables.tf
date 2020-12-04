9710e203453f7b69b1f9e2c68c01226a3d6f4291
kaushikarvind <56485562+kaushikarvind@users.noreply.github.com>
2020-12-04 20:56:02 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,7 +14,22 @@ variable "tenant_id" {
     description = "Enter tenant ID"  
 }
 
-variable region {
-    description = "AWS Region"
-    default = "us-east-1"
+variable az_location {
+    description = "Azure Region"
+    default = "UK South"
+}
+
+variable az_rg {
+    description = "Azure Resource Group"
+    default = "mb-rg"
+}
+
+variable az_packer_image {
+    description = "Azure Image created by Packer"
+    default = "mb-rhel-dec20"
+}
+
+variable az_tag_env {
+    description = "Environment Tag"
+    default = "MB TF Demo"
 }
