32c2fedcd4b96761d0003c41dfdcc5b2095db52a
kaushikarvind <56485562+kaushikarvind@users.noreply.github.com>
2020-12-04 20:55:25 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,29 +20,29 @@ provider "azurerm" {
 //}
 
 # Create a resource group if it doesn't exist
-resource "azurerm_resource_group" "tf_rg" {
-  name     = "mb-rg"
-  location = "UK South"
-  tags = {
-    environment = "MB TF demo"
-  }
-}
+#resource "azurerm_resource_group" "tf_rg" {
+#  name     = "mb-rg"
+#  location = "UK South"
+#  tags = {
+#    environment = var.az_tag_env
+#  }
+#}
 
 # Create virtual network
 resource "azurerm_virtual_network" "tf_vn" {
   name                = "mb-vn"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.tf_rg.location
-  resource_group_name = azurerm_resource_group.tf_rg.name
+  location            = var.az_location
+  resource_group_name = var.az_rg
   tags = {
-    environment = "MB TF demo"
+    environment = var.az_tag_env
   }
 }
 
 # Create subnet
 resource "azurerm_subnet" "tf_sub" {
   name                 = "mb-sub"
-  resource_group_name  = azurerm_resource_group.tf_rg.name
+  resource_group_name  = var.az_rg
   virtual_network_name = azurerm_virtual_network.tf_vn.name
   address_prefixes     = ["10.0.2.0/24"]
 }
@@ -50,20 +50,20 @@ resource "azurerm_subnet" "tf_sub" {
 # Create public IPs
 resource "azurerm_public_ip" "tf_pub_ip" {
   name                = "mb-pub-ip"
-  resource_group_name = azurerm_resource_group.tf_rg.name
-  location            = azurerm_resource_group.tf_rg.location
+  resource_group_name = var.az_rg
+  location            = var.az_location
   allocation_method   = "Dynamic"
 
   tags = {
-    environment = "MB TF demo"
+    environment = var.az_tag_env
   }
 }
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "tf_sg" {
     name                = "mb-sg"
-    location            = azurerm_resource_group.tf_rg.location
-    resource_group_name = azurerm_resource_group.tf_rg.name
+    location            = var.az_location
+    resource_group_name = var.az_rg
 
     security_rule {
         name                       = "SSH"
@@ -78,15 +78,15 @@ resource "azurerm_network_security_group" "tf_sg" {
     }
 
     tags = {
-        environment = "MB TF Demo"
+        environment = var.az_tag_env
     }
 }
 
 # Create network interface
 resource "azurerm_network_interface" "tf_ni" {
   name = "mb-nic"
-  location = azurerm_resource_group.tf_rg.location
-  resource_group_name = azurerm_resource_group.tf_rg.name
+  location = var.az_location
+  resource_group_name = var.az_rg
 
   ip_configuration {
     name = "mb-pvt-ip"
@@ -95,7 +95,7 @@ resource "azurerm_network_interface" "tf_ni" {
     public_ip_address_id = azurerm_public_ip.tf_pub_ip.id
   }
   tags = {
-        environment = "MB TF Demo"
+        environment = var.az_tag_env
     }
 }
 
@@ -109,7 +109,7 @@ resource "azurerm_network_interface_security_group_association" "tf-ni-sg-assoc"
 resource "random_id" "tf_randomId" {
     keepers = {
         # Generate a new ID only when a new resource group is defined
-        resource_group = azurerm_resource_group.tf_rg.name
+        resource_group = var.az_rg
     }
     byte_length = 8
 }
@@ -117,13 +117,13 @@ resource "random_id" "tf_randomId" {
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "tf_storageaccount" {
     name                        = "diag${random_id.tf_randomId.hex}"
-    resource_group_name         = azurerm_resource_group.tf_rg.name
-    location                    = azurerm_resource_group.tf_rg.location
+    resource_group_name         = var.az_rg
+    location                    = var.az_location
     account_tier                = "Standard"
     account_replication_type    = "LRS"
 
     tags = {
-        environment = "MB TF Demo"
+        environment = var.az_tag_env
     }
 }
 
@@ -134,17 +134,22 @@ resource "tls_private_key" "tf_ssh" {
 }
 output "tls_private_key" { value = tls_private_key.tf_ssh.private_key_pem }
 
+resource "local_file" "azureuser_pem" {
+  content  = tls_private_key.tf_ssh.private_key_pem
+  filename = "azureuser.pem"
+}
+
 # Reference the VM image created by Packer
 data "azurerm_image" "packer_image" {
-  name = "MB_dec20"
-  resource_group_name = "tf_rg"
+  name = var.az_packer_image
+  resource_group_name = var.az_rg
 }
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "tf_vm1" {
-  name                = "mb-vm1"
-  resource_group_name = azurerm_resource_group.tf_rg.name
-  location            = azurerm_resource_group.tf_rg.location
+  name                = "mb-rhel-vm1"
+  resource_group_name = var.az_rg
+  location            = var.az_location
   size                = "Standard_B1ls"
 //  admin_username      = "adminuser"
   network_interface_ids = [azurerm_network_interface.tf_ni.id,]
@@ -171,7 +176,7 @@ resource "azurerm_linux_virtual_machine" "tf_vm1" {
   # #id = azurerm_image.packer_image.id
   # }
 
-  computer_name  = "mb-vm1"
+  computer_name  = "mb-rhel-vm1"
   admin_username = "azureuser"
   disable_password_authentication = true
 
@@ -185,6 +190,6 @@ resource "azurerm_linux_virtual_machine" "tf_vm1" {
     }
 
     tags = {
-      environment = "MB TF Demo"
+      environment = var.az_tag_env
     }
 }
