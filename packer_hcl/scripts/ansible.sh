64337f41fa63478cc1dad3b10f4eabd2f5c0834a
kaushikarvind <56485562+kaushikarvind@users.noreply.github.com>
2020-12-04 20:23:56 +0000

    Update ansible.sh

--- packer_hcl/scripts/ansible.sh
+++ packer_hcl/scripts/ansible.sh
@@ -4,7 +4,9 @@
 #sudo apt-get -y update && sudo apt-get -y upgrade
 #sudo apt-get -y install software-properties-common
 #sudo apt-add-repository ppa:ansible/ansible
-sudo yum install -y rpm
+sudo yum -y install rpm
+#sudo rpm -i epel-release-latest-7.noarch.rpm
+#sudo subscription-manager repos --enable rhel-7-server-ansible-2.9-rpms
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum -y update && sudo yum -y upgrade
 
